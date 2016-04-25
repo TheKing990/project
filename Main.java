1e04081f8ff10793ee09e90bb6b26e247400f259
Jhovany Gonzalez <jhovany990@gmail.com>
2016-04-24 19:33:52 -0500

    fix error

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException
 	{
 		// TODO Auto-generated method stub
        		Atm m =  new Atm();
@@ -11,44 +15,43 @@ public class Main {
 		String userName = "aws1";
 		String password = "passw0rd";
 		String dbName = "Java";
-		
-		Class.forName("com.mysql.jdbc.Driver").newInstance();
-		Connection connection = DriverManager.getConnection(url + dbName , userName, password);
-		Statement st = connection.createStatement();
-	
-		//should work like regular sql, maybe a bit slower though.
-		st.executeUpdate("update bank set firstname = 'Jane' where pin = '7777'");
-		
-		ResultSet rs = st.executeQuery("select * from bank");
-		//table has 5 columns (in order): id(12 Varchar), pin(4 varchar), balance(int), lastname(25 varchar), and firstname(25 varchar).
-		//if you want me to modify the table in any way let me know. Other banks can be individual tables or databases, let me know what you
-		//would prefer.
-		while(rs.next())
-		{
-			System.out.println(rs.getString(1) +" "+ rs.getString(2) +" "+ rs.getString(3) +" "+rs.getString(4)
-			+" "+ rs.getString(5));
-		}
-		System.out.println();
-		
-		//Test to display contents of second bank
-		rs = st.executeQuery("select * from bank2");
-		while(rs.next())
-		{
-			System.out.println(rs.getString(1) +" "+ rs.getString(2) +" "+ rs.getString(3) +" "+rs.getString(4)
-			+" "+ rs.getString(5));
-		}	
-		System.out.println();
-		
-		//Test to display contents of third bank
-		rs = st.executeQuery("select * from bank3");
-		while(rs.next())
-		{
-			System.out.println(rs.getString(1) +" "+ rs.getString(2) +" "+ rs.getString(3) +" "+rs.getString(4)
-			+" "+ rs.getString(5));
-		}
-		System.out.println();
 
-		connection.close();
+
+
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Connection connection = DriverManager.getConnection(url + dbName, userName, password);
+            Statement st = connection.createStatement();
+
+            //should work like regular sql, maybe a bit slower though.
+            st.executeUpdate("update bank set firstname = 'Jane' where pin = '7777'");
+
+            ResultSet rs = st.executeQuery("select * from bank");
+            //table has 5 columns (in order): id(12 Varchar), pin(4 varchar), balance(int), lastname(25 varchar), and firstname(25 varchar).
+            //if you want me to modify the table in any way let me know. Other banks can be individual tables or databases, let me know what you
+            //would prefer.
+            while (rs.next()) {
+                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4)
+                        + " " + rs.getString(5));
+            }
+            System.out.println();
+
+            //Test to display contents of second bank
+            rs = st.executeQuery("select * from bank2");
+            while (rs.next()) {
+                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4)
+                        + " " + rs.getString(5));
+            }
+            System.out.println();
+
+            //Test to display contents of third bank
+            rs = st.executeQuery("select * from bank3");
+            while (rs.next()) {
+                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4)
+                        + " " + rs.getString(5));
+            }
+            System.out.println();
+
+
 		
 		//If you get this it works
 		System.out.println("Test complete");
